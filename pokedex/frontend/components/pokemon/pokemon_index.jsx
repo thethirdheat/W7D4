0412e4ddfbbe0c19d3d2367c61d1c675e493f171
thethirdheat <rockstrongo22@gmail.com>
2019-06-26 17:35:36 -0700

    Start PokemonIndex page component

---
+++ pokedex/frontend/components/pokemon/pokemon_index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PokemonIndex extends React.Component{
+  constructor(props){
+    // console.log(props);
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>Pokemon Index Page</div>
+    );
+  }
+
+}
+
+export default PokemonIndex;
--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from '../frontend/components/root';
 
+//for testing
 import { fetchAllPokemon } from './util/api_util';
-import { receiveAllPokemon } from './actions/pokemon_actions';
+import { receiveAllPokemon , requestAllPokemon } from './actions/pokemon_actions';
+import { selectAllPokemon } from './reducers/selectors';
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
 
-  const store = configureStore();
-  
+  let store = configureStore();
+
+  //testing
   window.getState = store.getState;
   window.dispatch = store.dispatch;
 
   window.fetchAllPokemon = fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
+  window.selectAllPokemon = selectAllPokemon;
 
-  ReactDOM.render(<div> our pokedex </div>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
